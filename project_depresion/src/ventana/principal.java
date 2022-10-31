f2c68359bc5a6fc5e74d1581892b7a667f233486
Coke-GalPi <scoutkoke@gmail.com>
2022-10-31 18:41:35 -0300

    Creación de panel y etiqueta

--- project_depresion/src/ventana/principal.java
+++ project_depresion/src/ventana/principal.java
@@ -3,8 +3,7 @@ package ventana;
 public class principal {
 
 	public static void main(String[] args) {
-		ventana v1 = new ventana ();
+		ventana v1 = new ventana();
 		v1.setVisible(true); // Hacemos visible la ventana
 	}
-	
 }
