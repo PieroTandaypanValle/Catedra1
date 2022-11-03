fdea4c2998fbb6b90f1179e3b1a3da1ab591c603
Coke-GalPi <scoutkoke@gmail.com>
2022-11-03 17:46:05 -0300

    Cambio en la idea del programa

--- project_depresion/src/ventana/principal.java
+++ project_depresion/src/ventana/principal.java
@@ -1,9 +1,44 @@
 package ventana;
 
-public class principal {
+import java.util.*;
 
+public class principal {
+	public static void imprimir_menu(){
+		System.out.println("\t<<Menu>>");
+		System.out.println("0.- Salir del Programa");
+		System.out.println("1.- Ingresar Paciente");
+		System.out.println("2.- Realizar Formulario");
+		System.out.println("3.- Actualizar Formulario");
+		System.out.println("4.- Guardar");
+	}
 	public static void main(String[] args) {
-		ventana v1 = new ventana();
-		v1.setVisible(true); // Hacemos visible la ventana
+		/*ventana v1 = new ventana();
+		v1.setVisible(true); // Hacemos visible la ventana*/
+		Scanner Sc = new Scanner (System.in);
+		boolean salir_menu = true;
+        do{
+			imprimir_menu();
+            System.out.print("Seleccione una de las opciones del menu: ");
+            int opcion = Sc.nextInt();              // Pide un numero 
+            switch (opcion){
+                case 0:
+                    salir_menu = false;
+                break;
+                case 1:
+                    System.out.println("\t <Igresar Paciente>");
+                break;
+                case 2:
+                    System.out.println("\t <Realizar Formulario>");
+                break;
+                case 3:
+                    System.out.println("\t <Actualizar Formulario>");
+                break;
+                case 4:
+                    System.out.println("\t <Guardar>");
+                break;
+                default : System.out.println("Error, opción fuera de rango");
+            }
+        } while (salir_menu);
+        System.out.println("Acabas de salir del programa");
 	}
 }
