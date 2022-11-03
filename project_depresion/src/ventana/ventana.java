3803764428092ea36430bd5739c9689070ab805f
Coke-GalPi <scoutkoke@gmail.com>
2022-11-03 13:16:50 -0300

    Asignación de puntos, otros

--- project_depresion/src/ventana/ventana.java
+++ project_depresion/src/ventana/ventana.java
@@ -3,7 +3,6 @@ package ventana;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import java.awt.Color;
 
 public class ventana extends JFrame {
@@ -12,7 +11,7 @@ public class ventana extends JFrame {
 		setSize(600, 600); // Se asigna Alto y Ancho a la ventana
 		setTitle("Formulario para 'Red Flag' de Depresion"); // Se asigna Titulo a la ventana
 		setLocationRelativeTo(null); // Se centra al tamaño de la ventana
-		this.getContentPane().setBackground(new Color(9, 9, 9)); // A la ventana se le asigna el color #090909
+		//this.getContentPane().setBackground(new Color(9, 9, 9)); // A la ventana se le asigna el color #090909
 		iniciar_componentes();
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE); // Termina el programa
